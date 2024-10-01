597792f45d6cac63032a97c9beb4f01cc2e65908
elizabethlconn <148993813+elizabethlconn@users.noreply.github.com>
2024-10-01 13:19:44 -0400

    Repository setup

---
+++ static/js/app.js
@@ -0,0 +1,87 @@
+// Build the metadata panel
+function buildMetadata(sample) {
+  d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
+    // console.log(data);
+
+    // get the metadata field
+    let metadata = data.metadata;
+
+    // Filter the metadata for the object with the desired sample number
+    let resultArray = metadata.filter(metaObj => metaObj.id.toString() === sample);
+    let result = resultArray[0];
+
+    // Use d3 to select the panel with id of `#sample-metadata`
+    let panel = d3.select(`#sample-metadata`);
+
+    // Use `.html("") to clear any existing metadata
+    panel.html("");
+
+
+    // Inside a loop, you will need to use d3 to append new
+    // tags for each key-value in the filtered metadata.
+
+  });
+}
+
+// function to build both charts - buildChart using buildMetadata
+function buildCharts(sample) {
+  d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
+
+    // Get the samples field
+
+
+    // Filter the samples for the object with the desired sample number
+
+
+    // Get the otu_ids, otu_labels, and sample_values
+
+
+    // Build a Bubble Chart
+
+
+    // Render the Bubble Chart
+
+
+    // For the Bar Chart, map the otu_ids to a list of strings for your yticks
+
+
+    // Build a Bar Chart
+    // Don't forget to slice and reverse the input data appropriately
+
+
+    // Render the Bar Chart
+
+  });
+}
+
+// Function to run on page load
+function init() {
+  d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
+
+    // Get the names field
+
+
+    // Use d3 to select the dropdown with id of `#selDataset`
+
+
+    // Use the list of sample names to populate the select options
+    // Hint: Inside a loop, you will need to use d3 to append a new
+    // option for each sample name.
+
+
+    // Get the first sample from the list
+
+
+    // Build charts and metadata panel with the first sample
+
+  });
+}
+
+// Function for event listener
+function optionChanged(newSample) {
+  // Build charts and metadata panel each time a new sample is selected
+
+}
+
+// Initialize the dashboard
+init();
